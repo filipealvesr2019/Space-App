ee740163fc05a31e62dfd10a66ca363f0b0caa30
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-10-07 20:01:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <style> </style>
+
+      <h1>The Design System</h1>
+
+      <img src="the-design-system.jpg" alt="the design system"></img>
+    </>
+  );
+}
+
+export default App;
