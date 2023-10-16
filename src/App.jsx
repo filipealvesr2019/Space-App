65b15603f1a9fb16a911129340c13c961333ccf0
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-10-16 13:28:32 -0300

    cm

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,54 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div  className="container">
-        <h1>Design system</h1>
-        <section id="colors" ></section>
+      <div className="wrapper">
+        <div className="container text-white">
+          <h1>Design system</h1>
+          <h2 className="h2">
+              <span>01</span>
+            </h2>
+          <section id="colors">
+            <div className="color1">
+           
+            <div>
+              <div
+                className="bg-dark text-white"
+                style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
+              > #0B0D17</div>
+             
+            </div>
+            <p><span className="text-accent">RGB</span> 11, 13, 23</p>
+            <p><span className="text-accent">HSL</span> 230°, 35%, 7%</p>
+            </div>
+
+
+            <div className="color2">
+            <div>
+              <div
+                className="bg-accent text-dark"
+                style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
+              >#0B0D17</div>
+              
+            </div>
+            <p><span className="text-accent">RGB</span> 11, 13, 23</p>
+            <p><span className="text-accent">HSL</span> 230°, 35%, 7%</p>
+            </div>
+
+
+            <div className="color3">
+            <div>
+              <div
+                className="bg-white text-dark"
+                style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}
+              >#FFFFFF</div>
+              
+            </div>
+            <p><span className="text-accent">RGB</span> 11, 13, 23</p>
+            <p><span className="text-accent">HSL</span> 230°, 35%, 7%</p>
+            </div>
+           
+          </section>
+        </div>
       </div>
     </>
   );
